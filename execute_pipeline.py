07cd257b12063e0ae0cce5bc0c048d2963e454b7
Levy Nunes <levy.vix@gmail.com>
2023-07-26 13:06:48 -0300

    separar extract e transform

--- execute_pipeline.py
+++ execute_pipeline.py
@@ -1,13 +1,15 @@
 import papermill as pm
 
+
 pm.execute_notebook(
     "pegar_dados_youtube.ipynb",
     "pegar_dados_youtube.ipynb",
     parameters=dict(),
 )
 
+
 pm.execute_notebook(
-    "criar_tabela_unica.ipynb",
-    "criar_tabela_unica.ipynb",
+    "transform.ipynb",
+    "transform.ipynb",
     parameters=dict(),
 )
