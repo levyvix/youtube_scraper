d3222d56ce260852652c4eb634da116f516dae3a
Levy Nunes <levy.vix@gmail.com>
2023-11-16 13:26:36 -0300

    fix

--- execute_pipeline.py
+++ execute_pipeline.py
@@ -1,8 +1,10 @@
 import papermill as pm
 
 
-pm.execute_notebook(
-    "pegar_dados_youtube.ipynb", "pegar_dados_youtube.ipynb", parameters={}
-)
+# pm.execute_notebook(
+#     "pegar_dados_youtube - dev.ipynb",
+#     "pegar_dados_youtube - dev - out.ipynb",
+#     parameters={},
+# )
 
 pm.execute_notebook("transform.ipynb", "transform.ipynb", parameters={})
