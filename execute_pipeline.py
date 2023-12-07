4029397ea92ba9ce81459e2628cc9dfe6a6fc098
Levy Nunes <levy.vix@gmail.com>
2023-12-07 15:17:47 -0300

    atualizado

--- execute_pipeline.py
+++ execute_pipeline.py
@@ -1,10 +1,12 @@
 import papermill as pm
 
+# antes de rodar, instalar os pacotes
+# pip install -r requirements.txt
 
-# pm.execute_notebook(
-#     "pegar_dados_youtube - dev.ipynb",
-#     "pegar_dados_youtube - dev - out.ipynb",
-#     parameters={},
-# )
+pm.execute_notebook(
+    "pegar_dados_youtube - dev.ipynb",
+    "pegar_dados_youtube - dev - out.ipynb",
+    parameters={},
+)
 
 pm.execute_notebook("transform.ipynb", "transform.ipynb", parameters={})
